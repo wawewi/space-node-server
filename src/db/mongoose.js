94fb69a190899c8f9f19ab9c8e66b5fc75feb0a4
winston-lim <winston_lim1@hotmail.com>
2021-03-16 15:29:50 +0800

    Build auth middleware to verify tokens

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,19 +4,3 @@ mongoose.connect(process.env.MONGODB_URL, {
   useUnifiedTopology: true,
   useCreateIndex: true, 
 });
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  }
-})
-
-const user1 = new User({
-  name: 'Weng',
-  age: 27,
-})
-
-user1.save().then(console.log).catch(console.log);
---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+//Check if token is genuine
+const auth = async (req, res, next) => {
+  try {
+    //Bearer tokens will be attached to a header where key is 'Authorization; and value is `Bearer ${token}`
+
+    //removes 'Bearer ' to get token
+    const token = req.header('Authorization').replace('Bearer ', '');
+
+    //Validates if token is genuine, if so it returns the decoded token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //After verifying token authencity, find User it belongs to - it should have same _id property and exist in the tokens array
+    const user = await User.findOne({_id: decodedToken._id, 'tokens.token': token});
+    
+    if (!user) {
+      throw new Error('Not authorized to perform operation')
+    }
+
+    //attach token and user to request object for following middleware
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send(e);
+  }
+}
+
+module.exports = auth;
